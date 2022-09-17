1aee929ee5ef87418adacb295053acd65b140c81
jessicamelise <jessica.melise@hotmail.com>
2022-09-17 15:24:49 -0300

    chore: set global var to test deploy crete db

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ app = FastAPI()
 
 @app.get("/", status_code=200)
 def test():
+    global DATABASE_URL
     print('teste', DATABASE_URL)
     if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
